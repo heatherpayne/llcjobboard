d1777d39f73eb7ab479ef0c471047d5c6d14eb6c
Heather Payne <heather@heatherpayne.ca>
2013-05-15 23:47:43 -0400

    update to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,5 +5,6 @@ class Admin::DashboardController < ApplicationController
   def index
     @job_postings = JobPosting.all
     @categories = Category.all
+    @featured_jobs = JobPosting.find(:is_featured => :true)
   end
 end
